85f8e9c49b796f4ede74373217a931881df4a90b
daehwan0307 <eoghks960307@naver.com>
2022-11-27 18:48:41 +0900

    project 정리

--- src/main/java/com/example/springbootjpa/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootjpa.repository;
-
-import com.example.springbootjpa.domain.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author,Long> {
-}
